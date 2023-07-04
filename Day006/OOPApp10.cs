118f61ff590cb902f1ff2c36ad309898979eb2ad
hjnam0117 <62553647+hjnam0117@users.noreply.github.com>
2023-07-04 15:49:20 +0900

    Create OOPApp10.cs

---
+++ Day006/OOPApp10.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPApp10
+{
+    abstract class Juice
+    {
+        public int Level { get; set; }
+        public abstract string Drink();
+    }
+    class Ade : Juice
+    {
+        public override string Drink()
+        {
+            return "마시다";
+        }
+    }
+    class Lemonade : Ade 
+    { 
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Lemonade j = new Lemonade();
+            Console.WriteLine(j.Drink());
+        }
+    }
+}
