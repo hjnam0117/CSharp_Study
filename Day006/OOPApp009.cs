f03c59b538acd902a48afbc296ad51fbfdf166d8
hjnam0117 <62553647+hjnam0117@users.noreply.github.com>
2023-07-04 15:27:05 +0900

    Create OOPApp009.cs

---
+++ Day006/OOPApp009.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPApp009
+{
+    abstract class Shape
+    {
+        public abstract void Draw(); //추상 메소드는 구현x 
+    }
+    class Triangle : Shape
+    {
+        public override void Draw()
+        {
+            Console.WriteLine("삼각형을 그리다.");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //실세계
+            Triangle t = new Triangle();
+            t.Draw();
+
+            Shape s = new Triangle();
+            s.Draw();
+        }
+    }
+}
