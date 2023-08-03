b47cd8a8956f72eba4740c2ccfd7014f045e4276
hjnam0117 <62553647+hjnam0117@users.noreply.github.com>
2023-08-03 11:18:43 +0900

    Create ConsoleTCPClient.cs

---
+++ Day028/ConsoleTCPClient.cs
@@ -0,0 +1,30 @@
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleTCPClient
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string server = "192.168.36.98";
+            int port = 13000;
+
+            TcpClient client = new TcpClient(server, port);
+
+            NetworkStream stream = client.GetStream();
+
+            byte[] data = new byte[256];
+            int bytes = stream.Read(data, 0, data.Length);
+            string responseData = Encoding.UTF8.GetString(data, 0, bytes);
+            Console.WriteLine($"Received: {responseData}");
+
+            client.Close();
+        }
+    }
+}
